07fd943facbb9cdc70036b362a950291cd8c3b07
Monkeyuan <1571610275@qq.com>
2015-10-30 00:32:15 -0400

    sddd

---
+++ process/src/com/yuan/groovy/test/helloWorld.groovy
@@ -0,0 +1,18 @@
+package com.yuan.groovy.test
+
+/**
+ * Created by Yuan on 2015/10/30.
+ */
+class helloWorld {
+    def  test = "Hello World"
+    helloWorld(){}
+
+    static void main(String[] args){
+
+        println ("hello world")
+    }
+
+
+
+
+}
